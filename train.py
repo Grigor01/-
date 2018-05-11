d1dd63eb6294f2557342434d9e75788cfdf3a3fd
Grigor01 <37597923+Grigor01@users.noreply.github.com>
2018-05-11 19:49:35 +0300

    Ready

--- train.py
+++ train.py
@@ -6,10 +6,10 @@ import pickle
 import glob
 from collections import defaultdict
 
-Lang = re.compile(u'[а-яА-Я]+')
+lang = re.compile(u'[а-яА-Я]+')
 
 
-def LinesGen(text, reg):
+def lines_gen(text, reg):
     for line in text:
         if reg:
             yield line.lower()
@@ -17,13 +17,13 @@ def LinesGen(text, reg):
             yield line
 
 
-def TokensGen(lines):
+def tokens_gen(lines):
     for line in lines:
-        for token in Lang.findall(line):
+        for token in lang.findall(line):
             yield token
 
 
-def TrigramsGen(tokens):
+def trigrams_gen(tokens):
     w1, w2 = '7', '7'
     for w3 in tokens:
         yield w1, w2, w3
@@ -32,9 +32,9 @@ def TrigramsGen(tokens):
 
 def train(text, reg):
     model = {}
-    MyLines = LinesGen(text, reg)
-    MyTokens = TokensGen(MyLines)
-    MyTrigrams = TrigramsGen(MyTokens)
+    MyLines = lines_gen(text, reg)
+    MyTokens = tokens_gen(MyLines)
+    MyTrigrams = trigrams_gen(MyTokens)
     bigramm, trigramm = defaultdict(lambda: 0.0), defaultdict(lambda: 0.0)
 
     for w1, w2, w3 in MyTrigrams:
